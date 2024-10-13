61fd365408c7f1450ec95794f8ab862bb8c9045f
devguilhermeribeiro <devguilhermeribeiro@gmail.com>
2024-10-13 09:02:02 -0300

    Altering users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('document', 14)->unique();
+            $table->enum('document_type', ['cpf', 'cnpj']);
             $table->string('email')->unique();
+            $table->string('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
