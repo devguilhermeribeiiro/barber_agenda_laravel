2b301b6c1d17d07a1bd5a25aa8243c24567c1ec5
devguilhermeribeiro <devguilhermeribeiro@gmail.com>
2024-10-13 09:43:28 -0300

    Modified because laravel/ui implementation

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'document',
+        'document_type',
+        'address',
         'email',
         'password',
     ];
