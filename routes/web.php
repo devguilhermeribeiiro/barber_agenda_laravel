27dc427bce93b5ffc10d62a74e1d96f82f02bf55
devguilhermeribeiro <devguilhermeribeiro@gmail.com>
2024-10-18 22:42:06 -0300

    Setting routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('userRole');
